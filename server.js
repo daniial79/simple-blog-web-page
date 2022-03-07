eece85463bada4b831e33d9221bb6df725bf8cc5
danial <75265474+daniial79@users.noreply.github.com>
2022-03-07 18:45:36 +0330

    Update server.js
    update server configs

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = Number(process.env.PORT) || 5000;
 const hostName = process.env.SERVER_HOST || '127.0.0.1';
 
 //connect to database
-const dbUri = 'mongodb+srv://daniial:13454779d@practicenode.qfrfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dbUri = 'mongodb+srv://<username>:<password>@practicenode.qfrfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(result => server.listen(port, hostName, () => console.log(`server is live at http://${hostName}:${port} baseUrl`)))
     .catch(error => console.log('something went wrong!'));
